d40a11fd45f7f4f37f510de346f93f8aff2a7557
Teuton-Roy <popeyehk404@gmail.com>
2022-08-26 09:38:53 +0530

    java programs

--- src/TwoSumBoutForce.java
+++ src/TwoSumBoutForce.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TwoSumBoutForce {
     public static int[] twoSum(int[] arr, int target){
-        int[] temp = new int[];
+        int[] temp = new int[2];
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
