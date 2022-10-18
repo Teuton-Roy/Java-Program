50d55683b7e78269895222b4f8b128203b10b390
Teuton Roy <popeyehk404@gmaill.com>
2022-10-18 23:58:46 +0530

    java programs

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        //subtract two numbers//
+        int a = 10;
+        int b = 20;
+        int c = a-b;
+        System.out.println(c);
     }
 }
---
+++ src/reversString.java
@@ -0,0 +1,23 @@
+public class reversString {
+
+    //reverse a string//
+    public static String reverse(char[] s) {
+        int left = 0;            //point to first index//
+
+        int right = s.length - 1;//point to last index//
+
+        while (left < right) {   //swap the elements//
+            char temp = s[left]; //store the first element in temp//
+            s[left] = s[right];  //store the last element in first index//
+            s[right] = temp;     //store the first element in last index//
+            left++;              //increment the left index//
+            right--;             //decrement the right index//
+        }
+        return new String(s);    //return the string//
+    }
+
+    // Driver Code//
+    public static void main(String[] args) {
+        System.out.println(reverse("abc123".toCharArray())); //convert string to char array//
+    }
+}
